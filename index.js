69140c46dcc9dc051a9587faea11828621d0060b
SiriusKoan <k.peihsun@gmail.com>
2020-08-27 13:00:05 +0800

    add homepage animation

--- index.js
+++ index.js
@@ -1,14 +1,34 @@
-function typing_name() {
+function homepage_animation() {
+    // avatar
+    function show_avatar() {
+        avatar = document.getElementById("avatar");
+        avatar.style.opacity = 1;
+    }
+
+    // typing name
     var i = 0;
     var name = "SiriusKoan";
-    var speed = 60;
-
     function typing_effect() {
         if (i < name.length) {
             document.getElementById("typing-name").innerHTML += name.charAt(i);
             i++;
-            setTimeout(typing_effect, speed);
+            setTimeout(typing_effect, 100);
         }
     }
-    typing_effect();
+
+    // icon
+    function show_icon(params) {
+        document.getElementById("github").style.opacity = 1;
+        document.getElementById("telegram").style.opacity = 1;
+    }
+
+    // copyright
+    function show_copyright() {
+        document.getElementById("copyright").style.opacity = 1;
+    }
+
+    show_avatar();
+    setTimeout(typing_effect, 2000);
+    setTimeout(show_icon, 3000);
+    setTimeout(show_copyright, 4000);
 }
