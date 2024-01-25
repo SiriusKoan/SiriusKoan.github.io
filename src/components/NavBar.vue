beca9dfab81489f02132930483c159a1376b7a1b
SiriusKoan <me@siriuskoan.one>
2024-01-25 11:42:06 -0500

    feat: add blog link

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,7 +1,8 @@
 <template>
     <nav class="px-5 py-7 select-none underline-offset-4 bg-[#5B8BAD] top-0">
-        <RouterLink to="/" active-class="active" class="text-white decoration-solid ml-4 hover:opacity-70">SiriusKoan</RouterLink>
-        <RouterLink to="/about" active-class="active" class="text-white decoration-solid ml-4 hover:opacity-70">About Me</RouterLink>
+        <RouterLink to="/" active-class="active" class="link">SiriusKoan</RouterLink>
+        <RouterLink to="/about" active-class="active" class="link">About Me</RouterLink>
+        <a href="https://blog.siriuskoan.one" target="_blank" class="link">Blog</a>
     </nav>
 </template>
 
@@ -13,6 +14,10 @@ export default {
 
 <style scoped>
 nav .active {
-	text-decoration: underline;
+    @apply decoration-solid underline decoration-2;
+}
+
+.link {
+    @apply text-white decoration-solid ml-4 hover:underline decoration-2;
 }
 </style>
