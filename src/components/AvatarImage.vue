9647a873434df12a7e205159f4b401f1bed0642b
SiriusKoan <me@siriuskoan.one>
2024-01-25 22:56:46 -0500

    feat: christmas avatar

--- src/components/AvatarImage.vue
+++ src/components/AvatarImage.vue
@@ -1,17 +1,22 @@
 <template>
-    <div>
-        <img :src="avatar" class="rounded-full shadow-glow">
-    </div>
+  <div>
+    <img :src="avatar" class="rounded-full shadow-glow" />
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'AvatarImage',
-    data() {
-        return {
-            avatar: 'favicon.ico',
-        }
-    },
-}
+  name: "AvatarImage",
+  data() {
+    return {
+      avatar: "favicon.ico",
+    };
+  },
+  mounted() {
+    const date = new Date();
+    if (date.getMonth() === 11) {
+      this.avatar = "favicon-christmas.ico";
+    }
+  },
+};
 </script>
-
